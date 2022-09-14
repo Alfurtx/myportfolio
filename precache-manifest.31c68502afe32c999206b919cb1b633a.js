07125592dc4d78ec357514b5bd8731ae9cbcf691
Alfonso Amorós Cubells <alfonso.alfurtx@gmail.com>
2022-09-14 18:54:19 +0200

    Updates

---
+++ precache-manifest.31c68502afe32c999206b919cb1b633a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "832de60fb60046b38004",
+    "url": "/myportfolio/static/css/main.594572c1.chunk.css"
+  },
+  {
+    "revision": "832de60fb60046b38004",
+    "url": "/myportfolio/static/js/main.832de60f.chunk.js"
+  },
+  {
+    "revision": "0b6c132f8045ae7885ce",
+    "url": "/myportfolio/static/js/1.0b6c132f.chunk.js"
+  },
+  {
+    "revision": "6b6f225bb9dac2e07552",
+    "url": "/myportfolio/static/js/runtime~main.6b6f225b.js"
+  },
+  {
+    "revision": "bba42ab722820e5581f325dea0842c57",
+    "url": "/myportfolio/static/media/curriculum-test.bba42ab7.jpg"
+  },
+  {
+    "revision": "74287e944829a8fc0ec20b603e1ffea0",
+    "url": "/myportfolio/static/media/crane.74287e94.png"
+  },
+  {
+    "revision": "26de46e25f76be9ec6c436e0a58288dc",
+    "url": "/myportfolio/index.html"
+  }
+];
